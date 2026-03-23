de667bf2667b58fe3f9ec0356c9baca374586d91
Fatther <pavelanikevic@gmail.com>
2026-03-23 13:57:29 +0300

    a bit

--- src/main/java/pavel/lab/cinema/dto/error/ErrorResponse.java
+++ src/main/java/pavel/lab/cinema/dto/error/ErrorResponse.java
@@ -1,10 +1,7 @@
 package pavel.lab.cinema.dto.error;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
 import java.time.LocalDateTime;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
         int status,
         String message,
