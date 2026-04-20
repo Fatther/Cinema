e86edb78ea70fe472d57c8709bc5a26f760acb74
Fatther <pavelanikevic@gmail.com>
2026-04-20 16:14:07 +0300

    docker 12

--- src/main/java/pavel/lab/cinema/config/WebConfig.java
+++ src/main/java/pavel/lab/cinema/config/WebConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @SuppressWarnings("java:S5122")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -18,5 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
-
+    @Bean
+    @SuppressWarnings("java:S4502")
+    public SecurityFilterChain filterChain(HttpSecurity http) throws SecurityException {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
 }
