6e4bac3c95c43597a2c642a6e84987278a576428
Fatther <pavelanikevic@gmail.com>
2026-04-20 16:05:04 +0300

    docker 10

--- src/main/java/pavel/lab/cinema/config/WebConfig.java
+++ src/main/java/pavel/lab/cinema/config/WebConfig.java
@@ -3,6 +3,7 @@ package pavel.lab.cinema.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,9 +19,9 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws SecurityException {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
